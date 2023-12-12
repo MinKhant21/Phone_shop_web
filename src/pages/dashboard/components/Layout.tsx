bf26c2966757151f6c9b1d40ef58ec7e76b4009e
KaungMinKhant <kaungminkhant.edka@gmail.com>
2023-12-12 14:56:59 +0630

    sidenav

--- src/pages/dashboard/components/Layout.tsx
+++ src/pages/dashboard/components/Layout.tsx
@@ -1,18 +1,20 @@
 import { Outlet, useLocation } from "react-router-dom";
 import { SwitchTransition , CSSTransition } from "react-transition-group"
+import SideNavbar from "./SideNavbar";
 function Layout() {
     
      const location = useLocation();
      return (
-         <>
-             <div className=''>
-                 <SwitchTransition>
-                     <CSSTransition timeout={200} classNames="fade" key={location.pathname}>
-                         <Outlet/>
-                     </CSSTransition>
-                 </SwitchTransition>
+          <>
+             <div className=' flex'>
+               <SideNavbar/>
+               <SwitchTransition>
+                    <CSSTransition timeout={200} classNames="fade" key={location.pathname}>
+                    <Outlet/>
+                    </CSSTransition>
+               </SwitchTransition>
              </div>
-         </>
+          </>
      )
  }
 
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,8 @@
-import Layout from "./components/Layout";
 
 export default function AdminDashBoard() {
   return (
     <>
-     <Layout/>
-     <h1>hahah</h1>
+    <h1>H</h1>
     </>
   )
 }
