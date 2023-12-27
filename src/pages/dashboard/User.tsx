2206fc0d84393b6145a43c8edb0afbb5c6b8f88b
KaungMinKhant <kaungminkhant.edka@gmail.com>
2023-12-27 16:02:36 +0630

    add user Page

---
+++ src/pages/dashboard/User.tsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import useGetUser from "../../hooks/useGetUser";
+import { useEffect } from "react";
+import Loading from '../../component/Loading'
+export default function UserList() {
+  let {loading,error,users,getUsers} = useGetUser()
+  useEffect(()=>{
+    getUsers()
+  },[])
+  return (
+    <>
+     {
+      !loading ?
+      <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+      <Link to={"/dashboard/user/create"} className=" cursor-pointer  px-3 w-15 py-2 text-white rounded-lg text-center mb-5 f bg-danger">
+        Add
+      </Link>
+      <br />
+      <div className="max-w-full overflow-x-auto">
+        <table className="w-full table-auto">
+          <thead>
+            <tr className="bg-gray-2 text-left dark:bg-meta-4">
+              <th className="min-w-[220px] py-4 px-4 font-medium text-black dark:text-white xl:pl-11">
+                UserName
+              </th>
+              <th className="min-w-[150px] py-4 px-4 font-medium text-black dark:text-white">
+                Invoice date
+              </th>
+              <th className="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">
+                Status
+              </th>
+              <th className="py-4 px-4 font-medium text-black dark:text-white">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              users.map((user : any)=>
+                  <UserList key={user.id} user={user}/>
+              )
+            }
+          </tbody>
+        </table>
+      </div>
+    </div> : <Loading/>
+     } 
+    </>
+  )
+}
--- src/pages/dashboard/components/UserFormHandle.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function UserFormHandle() {
-  return (
-    <div>UserFormHandle</div>
-  )
-}
